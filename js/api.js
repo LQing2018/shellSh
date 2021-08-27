1e8cfd3379b2c91da45118218aeb032ff0179059
LQing2018 <572298346@qq.com>
2021-08-27 10:49:25 +0800

    PROD

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-var h5api="https://api.hacitd.com/buyer-api-test"
-var h5seller="https://api.hacitd.com/seller-api-test"
-var baseApi="http://zxkwx-boot.hacitd.com/test-wx-act"
-var henanapi="https://ha.tdg10086.cn/test-wx-act"
+var h5api="https://api.hacitd.com/buyer-api"
+var h5seller="https://api.hacitd.com/seller-api"
+var baseApi="http://zxkwx-boot.hacitd.com/wx-act"
+var henanapi="https://ha.tdg10086.cn/wx-act"
