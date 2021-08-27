d6276b8ed5ee9f34a557857317cf5ac92dc72f98
LQing2018 <572298346@qq.com>
2021-08-27 09:48:24 +0800

    测试提交

--- js/api.js
+++ js/api.js
@@ -11,3 +11,19 @@ var baseApi = 'http://zxkwx-boot.hacitd.com/wx-act';
 var henanapi = 'https://ha.tdg10086.cn/wx-act';
 
 
+//         # h5base = `https://api.hacitd.com/base-api-test`
+
+//         sed -i 's/base-api/xxx/g' ./js/api.js
+//         # tail -f ./js/api.js
+
+//         # sed -i '/baseApi/d' ./js/api.js
+// #         line=`sed -n '/baseApi/p' ./js/api.js`
+// #         echo ${line}
+//         # 替换
+//         # sed '${line}c ``';' ./js/api.js
+// #         cat ./js/api.js 
+// #         echo "=== api已修改 ==="
+//         # h5api = "https://api.hacitd.com/buyer-api-test"
+//         # h5seller = 'https://api.hacitd.com/seller-api-test'
+//         # baseApi = 'http://zxkwx-boot.hacitd.com/wx-act'
+//         # henanapi = 'https://ha.tdg10086.cn/test-wx-act';
--- test.sh
+++ test.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
-if [[ $1 == 'test' ]]; then
-        # h5base = `https://api.hacitd.com/base-api-test`
-    echo $1
-#     sed -i 's/base-api/xxx/g' ./js/api.js
-        # tail -f ./js/api.js
-
-#         sed -i '/baseApi/d' ./js/api.js
-#         line=`sed -n '/baseApi/p' ./js/api.js`
-#         echo ${line}
-        # 替换
-        # sed '${line}c ``';' ./js/api.js
-#         cat ./js/api.js 
-#         echo "=== api已修改 ==="
-        # h5api = "https://api.hacitd.com/buyer-api-test"
-        # h5seller = 'https://api.hacitd.com/seller-api-test'
-        # baseApi = 'http://zxkwx-boot.hacitd.com/wx-act'
-        # henanapi = 'https://ha.tdg10086.cn/test-wx-act';
+if [[ $1 == "test" ]]; then
+# sed -i 's/base-api/xxx/g' ./js/api.js
+cat ./js/api.js 
+# echo $1
 else
         return 0
 fi
